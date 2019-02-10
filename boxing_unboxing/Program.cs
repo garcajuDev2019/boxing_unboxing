cf2b7d3ad9cb07567dc5b1a7ee25f69661b6e82d
garcajuvueling <garcajuvueling@gmail.com>
2019-02-10 11:39:29 +0100

    Agregar archivos de proyecto.

---
+++ boxing_unboxing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 10;
+
+            Console.WriteLine($"Value before boxing {value}");
+
+            object box = Boxing(value);
+
+            int value2 = Unboxing(box);
+
+            Console.WriteLine($"Value after boxing {value2}");
+        }
+
+        private static Object Boxing(int value)
+        {
+            Object box = value;
+            return box;
+        }
+
+        private static int Unboxing(object box) => (int)box;
+    }
+}
